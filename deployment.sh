25c545ffdfa4a81458865dfb4dbf8bb6f5f36cee
Efecan YILMAZ <efecanyilmaz@outlook.com>
2023-06-20 12:48:05 +0200

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -4,7 +4,8 @@
 # docker image name
 DOCKER_IMAGE_NAME="bulletinboard-ads-i562327"
 # docker image tag
-DOCKER_IMAGE_TAG="v4"
+# DOCKER_IMAGE_TAG="v4"
+DOCKER_IMAGE_TAG="$1"
 # registry url
 REGISTRY_URL="cc-ms-k8s-training.common.repositories.cloud.sap"
 
@@ -15,19 +16,17 @@ docker login -u "claude" -p "9qR5hbhm7Dzw6BNZcRFv" "$REGISTRY_URL"
 # docker push
 docker push "$REGISTRY_URL/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG"
 
-# k8 manifest apply (change placeholders)
+# create a new k8 deployment manifest for bulletinboard-ads using the template file, with the new docker image tag
+cd .k8s
+rm "temp_bulletinboard-ads.yml"
+sed "s/{parameterized-docker-image-tag}/$DOCKER_IMAGE_TAG/g" "3_bulletinboard-ads-template.yml" | tee "temp_bulletinboard-ads.yml"
+
 # apply d r secret
-kubectl apply -f ./.k8s/1_docker-registry.yml
+kubectl apply -f "1_docker-registry.yml"
 # apply db yaml
-kubectl apply -f ./.k8s/2_bulletinboard-ads-db.yml
+kubectl apply -f "2_bulletinboard-ads-db.yml"
 # apply app yaml
-kubectl apply -f ./.k8s/3_bulletinboard-ads.yml
+kubectl apply -f "temp_bulletinboard-ads.yml"
 
-#echo $(pwd)
-# TODO: dynamic parameter interception
-#   1 accept arguements inline for container image version
-#   2 write to bulletinboards-ads.yml as a parameter
-# TODO: if else check? to make it idempotent
-
-# finish echo ?
+cd ..
 echo "Deployment completed!"
